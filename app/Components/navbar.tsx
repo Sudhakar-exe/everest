da54f1d4100616b0773f3f7a28d7cd358909f10e
Sudhakar Chaulagain <sudhakar.chaulagain@selu.edu>
2024-09-29 00:25:21 -0500

    added routing

--- app/Components/navbar.tsx
+++ app/Components/navbar.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
     <>
@@ -6,13 +8,19 @@ export default function Navbar() {
           <img src="/Logo_de_Facebook.png" alt="Logo img"></img>
         </div>
         <div className="flex justify-center">
-          <button className=" p-4 px-6 hover:bg-green-700">Home</button>
-          <button className=" p-4 px-6 hover:bg-green-700">About</button>
-          <button className=" p-4 px-6 hover:bg-green-700">Help</button>
+          <button className=" p-4 px-6 hover:bg-green-700"><Link href="/">Home</Link></button>
+          <button className=" p-4 px-6 hover:bg-green-700">
+            <Link href="about">About</Link>
+          </button>
+          <button className=" p-4 px-6 hover:bg-green-700"><Link href="help">Help</Link></button>
         </div>
         <div className="flex justify-end">
           <form>
-            <input type="text" placeholder="Search Here" className="m-1 p-1 rounded-lg"></input>
+            <input
+              type="text"
+              placeholder="Search Here"
+              className="m-1 p-1 rounded-lg"
+            ></input>
           </form>
           <button className="m-1 p-1 bg-blue-500 rounded-lg hover:bg-blue-600 active:bg-blue-700">
             Profile
